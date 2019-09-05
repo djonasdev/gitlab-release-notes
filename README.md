7475a425e3c32e008556fb3af3a50439f07021ca
Dominic Jonas <djonas@haprotec.de>
2019-09-05 11:51:30 +0200

    Added script and README.md

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # gitlab-release-notes
 GitLab Changelog Generator with release notes
+
+---
+
+GitLab release notes are stored in the database, not in the Git repo while the message is stored in the Git repo. This is why you're able to get the message but not the release notes in the script you have.
+
+The only way you could get release notes would be to use the [GitLab API - Releases API](https://docs.gitlab.com/ee/api/releases/#get-a-release-by-a-tag-name).
+
+## Prerequisites
+
+In order to use the API you have to [create a personal access token](https://docs.gitlab.com/ee/user/profile/personal_access_tokens.html#creating-a-personal-access-token).
+
+## Modify script
+
+In the script is a `SETUP` section where you have to modify the gitlab url:
+
+```bash
+
+#####################################
+# Setup                             #
+#####################################
+
+$repoURL = 'https://gitlab.example.com'
+
+```
+
+## Run the script
+
+The following command is generating a changelog.txt file for your project with the `ID 13`.
+
+```console
+.\generateChangelog.ps1 -token 'mySecretToken' -projectID '13' > changelog.txt
+```
---
+++ generateChangelog.ps1
@@ -0,0 +1,60 @@
+# GitLab Changelog Generator with release notes
+# Author:Dominic Jonas
+
+# this script generates a simple changelog with the GitLab release notes
+# please modify the varaibles in the setup section
+
+#####################################
+# PARAMETER                         #
+#####################################
+
+param([string]$token, [int]$projectID)
+
+#####################################
+# Setup                             #
+#####################################
+
+$repoURL = 'https://gitlab.example.com'
+
+#####################################
+# Functions                         #
+#####################################
+
+Function SendWebRequest($request) {
+    $header = @{"PRIVATE-TOKEN"=$token}
+    $response = Invoke-WebRequest -Uri $request -Headers $header
+
+    # fix encoding
+    $encoded = [system.Text.Encoding]::UTF8.GetString($response.RawContentStream.ToArray()) | ConvertFrom-Json
+    return $encoded
+}
+
+#####################################
+# Logic                             #
+#####################################
+
+# get repo informations
+$repoJson = SendWebRequest($repoURL + '/api/v4/projects/' + $projectID)
+
+# get release informations
+$releasesJson = SendWebRequest($repoURL + '/api/v4/projects/' + $projectID + '/releases?per_page=100')
+
+
+# DEBUG print complete response
+#echo $repoJson
+
+# DEBUG print response object
+#echo $repoJson.psobject
+
+echo Changelog
+echo $repoJson.psobject.properties["name"].value
+echo $repoJson.psobject.properties["description"].value
+echo ----------------------
+
+# for loop foreach release
+foreach($r in $releasesJson){
+    echo ''
+    echo $r.name
+    echo $r.description
+}
+
